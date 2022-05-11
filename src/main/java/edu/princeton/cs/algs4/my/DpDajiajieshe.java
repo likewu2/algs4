cf4381a5d0614c0789b301845600cbcdbc6a0f8a
likewu <likewu@126.com>
2022-05-12 00:04:40 +0800

    aa

--- src/main/java/edu/princeton/cs/algs4/my/DpDajiajieshe.java
+++ src/main/java/edu/princeton/cs/algs4/my/DpDajiajieshe.java
@@ -4,25 +4,26 @@ import java.util.Arrays;
 
 public class DpDajiajieshe {
     public static void main(String[] args) {
-        int[] a= {7, 1, 5, 3, 6, 4, 6};
+        int[] a= {1, 2, 3, 1};
+        //int[] a= {2, 7, 9, 3, 1};
         System.out.println("a: "+ Arrays.toString(a));
         int ans = parse(a);
         System.out.println("ans: "+ans);
     }
 
     private static int parse(int[] a) {
-        if(a.length<2) return 0;
+        if(a.length<1) return 0;
+        if(a.length==1) return a[0];
+        if(a.length==2) return Math.max(a[0], a[1]);
 
-        int[][] dp=new int[a.length][2];
-        dp[0][0]=0;
-        dp[0][1]=-a[0];
-        for(int i=1;i<a.length;i++) {
-            dp[i][0]=Math.max(dp[i-1][0], dp[i-1][1]+a[i]);  //sell=max(sell, buy+a[i])
-            //dp[i][1]=Math.max(-a[i], dp[i-1][1]);  //buy=max(buy, -a[i])  //买入和卖出一次股票
-            dp[i][1]=Math.max(dp[i-1][0]-a[i], dp[i-1][1]);  //buy=max(buy, sell-a[i])  //买入和卖出多次股票
+        int[] dp=new int[a.length];
+        dp[0]=a[0];
+        dp[1]=Math.max(a[0], a[1]);
+        for(int i=2;i<a.length;i++) {
+            dp[i]=Math.max(dp[i-2]+a[i], dp[i-1]);
         }
 
         System.out.println("dp: "+Arrays.deepToString(dp));
-        return dp[a.length-1][0];
+        return dp[a.length-1];
     }
 }
